2e4a5a249e132bced5c6fac93eee8d6541834d1a
github username <github email address>
2019-09-06 18:40:11 -0700

    added rake task to scrape floristries website and seed database with rake command

--- app/models/Scrape.rb
+++ app/models/Scrape.rb
@@ -7,7 +7,14 @@ class Scrape < ApplicationRecord
     def self.get_floristries
         doc = Nokogiri::HTML(open('https://www.expertise.com/ma/boston/florists'))
         doc.css('h4.provider-card__header').each do |name|
+            #populate database with Floristries :)
             Floristry.create(name: name.text)
         end
+    end
+
+    def self.get_flowers
+        doc = Nokogiri::HTML(open('https://www.herinterest.com/15-most-popular-flowers-in-the-world/'))
+        binding.pry
+        doc.css('div#content-area.jpibfi_container')
     end
 end
